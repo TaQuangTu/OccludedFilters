07afa8916d7463a16de305d4aea9e30a085f6881
TaQuangTu <taquangtu132@gmail.com>
2021-01-21 18:14:56 +0700

    Filter and report to txt files

--- NuscenesHelper/NuscenesHelper.py
+++ NuscenesHelper/NuscenesHelper.py
@@ -8,28 +8,31 @@ class NuscenesHelper:
     def __init__(self, version: str = 'v1.0-mini', dataroot: str = '/data/sets/nuscenes'):
         self.nusc = NuScenes(version=version, dataroot=dataroot, verbose=True)
 
-    def filter_occluded_objects(self, saving_file_name, sensor='CAM_FRONT', report_file_path=None):
-        txt_file = open(saving_file_name, "w+")
-        # report_file = open(report_file_path,"w+")
-        # report_file.write("SENSOR="+sensor+"\n")
+    def filter_occluded_objects(self, occluded_tokens_file_path, sensor='CAM_FRONT', report_file_path=None):
+        occluded_tokens_file = open(occluded_tokens_file_path, "w+")
+        if report_file_path is not None:
+            report_file = open(report_file_path, "w+")
+            report_file.write("===================SENSOR=" + sensor + "============================\n")
         scenes = self.nusc.scene
         for scene in scenes:
             print("processing scene ", scene['token'])
-            # report_file.write("scene "+scene['token']+"\n")
-            occluded_tokens = self.get_occluded_annos_in_scence(scene, sensor)
-            for token in occluded_tokens:
-                txt_file.write(token + "\n")
-        txt_file.close()
+            occluded_anns = self.get_occluded_annos_in_scence(scene, sensor)
+            for ann in occluded_anns:
+                occluded_tokens_file.write(ann['token'] + "\n")
+            if report_file is not None:
+                for line in self.get_report_string(scene, occluded_anns):
+                    report_file.write(line + "\n")
+        occluded_tokens_file.close()
 
     def get_occluded_annos_in_scence(self, scene, sensor):
-        occluded_ann_tokens = []
+        occluded_anns = []
         samples = self.get_samples(scene)
         for sample in samples:
-            occluded_anno_tokens_in_sample = self.get_occluded_ann_tokens_in_sample(sample, sensor)
-            occluded_ann_tokens += occluded_anno_tokens_in_sample
-            print("----There are ", len(occluded_anno_tokens_in_sample), " in sample ", sample['token'], "of scene",
+            occluded_anns_in_sample = self.get_occluded_anns_in_sample(sample, sensor)
+            occluded_anns += occluded_anns_in_sample
+            print("----There are ", len(occluded_anns_in_sample), " in sample ", sample['token'], "of scene",
                   scene['token'])
-        return occluded_ann_tokens
+        return occluded_anns
 
     def get_samples(self, scene):
         samples = []
@@ -42,18 +45,25 @@ class NuscenesHelper:
             samples.append(sample)
         return samples
 
+    def get_anns_in_scene(self, scene):
+        samples = self.get_samples(scene)
+        annos = []
+        for sample in samples:
+            annos += self.get_annos(sample)
+        return annos
+
     '''
         TODO: take care the sensor parameter
     '''
 
-    def get_occluded_ann_tokens_in_sample(self, sample, sensor):
-        occluded_ann_tokens = []
+    def get_occluded_anns_in_sample(self, sample, sensor):
+        occluded_anns = []
         anns = self.get_annos(sample)
         camera_data = self.nusc.get('sample_data', sample['data'][sensor])
         for ann in anns:
             if self.is_occluded(ann, anns, camera_data):
-                occluded_ann_tokens.append(ann['token'])
-        return occluded_ann_tokens
+                occluded_anns.append(ann)
+        return occluded_anns
 
     def get_annos(self, sample):
         anns = []
@@ -98,13 +108,39 @@ class NuscenesHelper:
     def get_scenes(self):
         return self.nusc.scene
 
-    def make_report(self, sensor, scene, occluded_ann_tokens):
-        lines = []
-        lines.append()
-
     @staticmethod
     def is_occluded_2D(points1, points2):
         convex_hull = spatial.ConvexHull(np.concatenate((points1.T, points2.T)))
         if np.amin(convex_hull.vertices) > 7:
             return True
         return False
+
+    @staticmethod
+    def get_categories(anns):
+        categories = []
+        for ann in anns:
+            if ann['category_name'] not in categories:
+                categories.append(ann['category_name'])
+        return categories
+
+    @staticmethod
+    def get_number_of_anns_in_category(anns, category_name):
+        number = 0
+        for ann in anns:
+            if  ann['category_name']==category_name:
+                number+=1
+        return number
+
+    def get_report_string(self, scene, occluded_anns):
+        lines = []
+        all_annos = self.get_anns_in_scene(scene)
+        categories = NuscenesHelper.get_categories(all_annos)
+
+        lines.append("====scene " + scene['token'])
+        lines.append("     number of samples = " + str(len(self.get_samples(scene))))
+        lines.append("     number of anns = " + str(len(self.get_anns_in_scene(scene))))
+        for category in categories:
+            number_of_anns_in_category = NuscenesHelper.get_number_of_anns_in_category(all_annos, category)
+            number_of_anns_occluded_in_category = NuscenesHelper.get_number_of_anns_in_category(occluded_anns,category)
+            lines.append("          All "+category+" : " + str(number_of_anns_in_category)+", occluded : "+str(number_of_anns_occluded_in_category))
+        return lines
--- filter.py
+++ filter.py
@@ -2,10 +2,9 @@ from NuscenesHelper.NuscenesHelper import NuscenesHelper
 
 SENSORS = ["CAM_FRONT", 'CAM_BACK', 'CAM_BACK_LEFT', 'CAM_FRONT_LEFT', 'CAM_FRONT_RIGHT', 'CAM_BACK_RIGHT']
 if __name__ == "__main__":
-
-    txt_file = "occluded_object_tokens.txt"
+    occluded_ann_tokens_file = "occluded_object_tokens.txt"
     report_file = "report.txt"
     datasetHelper = NuscenesHelper(version='v1.0-mini', dataroot='data/set/nuscenes/v1.0-mini')
 
     for sensor in SENSORS:  # filter occluded objects for camera sensors
-        datasetHelper.filter_occluded_objects(sensor + txt_file, sensor, sensor + report_file)
+        datasetHelper.filter_occluded_objects(sensor + occluded_ann_tokens_file, sensor, "reports/" + sensor + "_" + report_file)
