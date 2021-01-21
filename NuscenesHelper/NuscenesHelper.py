b26d091fda61c4ea06f4a8a48f580d01a09e5850
TaQuangTu <taquangtu132@gmail.com>
2021-01-21 16:41:50 +0700

    Filter by projecting 3D points to 2D plane and using ConvexHull for all camera sensor

--- NuscenesHelper/NuscenesHelper.py
+++ NuscenesHelper/NuscenesHelper.py
@@ -1,29 +1,35 @@
 from nuscenes import NuScenes
 from nuscenes.utils.geometry_utils import view_points
 import numpy as np
+from scipy import spatial
+
 
 class NuscenesHelper:
     def __init__(self, version: str = 'v1.0-mini', dataroot: str = '/data/sets/nuscenes'):
         self.nusc = NuScenes(version=version, dataroot=dataroot, verbose=True)
 
-    def filter_occluded_objects(self, saving_file_name):
+    def filter_occluded_objects(self, saving_file_name, sensor='CAM_FRONT', report_file_path=None):
         txt_file = open(saving_file_name, "w+")
+        # report_file = open(report_file_path,"w+")
+        # report_file.write("SENSOR="+sensor+"\n")
         scenes = self.nusc.scene
         for scene in scenes:
-            print("processing scene ",scene['token'])
-            occluded_tokens = self.get_occluded_objects_in_scence(scene)
+            print("processing scene ", scene['token'])
+            # report_file.write("scene "+scene['token']+"\n")
+            occluded_tokens = self.get_occluded_annos_in_scence(scene, sensor)
             for token in occluded_tokens:
                 txt_file.write(token + "\n")
         txt_file.close()
 
-    def get_occluded_objects_in_scence(self, scene):
-        occluded_object_tokens = []
+    def get_occluded_annos_in_scence(self, scene, sensor):
+        occluded_ann_tokens = []
         samples = self.get_samples(scene)
         for sample in samples:
-            occluded_object_tokens_in_sample = self.get_occluded_object_tokens_in_sample(sample)
-            occluded_object_tokens += occluded_object_tokens_in_sample
-            print("----There are ",len(occluded_object_tokens_in_sample)," in sample ",sample['token'],"of scene",scene['token'])
-        return occluded_object_tokens
+            occluded_anno_tokens_in_sample = self.get_occluded_ann_tokens_in_sample(sample, sensor)
+            occluded_ann_tokens += occluded_anno_tokens_in_sample
+            print("----There are ", len(occluded_anno_tokens_in_sample), " in sample ", sample['token'], "of scene",
+                  scene['token'])
+        return occluded_ann_tokens
 
     def get_samples(self, scene):
         samples = []
@@ -40,25 +46,25 @@ class NuscenesHelper:
         TODO: take care the sensor parameter
     '''
 
-    def get_occluded_object_tokens_in_sample(self, sample, sensor="CAM_FRONT"):
-        occluded_object_tokens = []
-        objects = self.get_objects(sample)
+    def get_occluded_ann_tokens_in_sample(self, sample, sensor):
+        occluded_ann_tokens = []
+        anns = self.get_annos(sample)
         camera_data = self.nusc.get('sample_data', sample['data'][sensor])
-        for object in objects:
-            if self.is_occluded(object, objects, camera_data):
-                occluded_object_tokens.append(object['token'])
-        return occluded_object_tokens
+        for ann in anns:
+            if self.is_occluded(ann, anns, camera_data):
+                occluded_ann_tokens.append(ann['token'])
+        return occluded_ann_tokens
 
-    def get_objects(self, sample):
-        objects = []
+    def get_annos(self, sample):
+        anns = []
         ann_tokens = sample["anns"]
         for ann_token in ann_tokens:
-            object = self.nusc.get("sample_annotation", ann_token)
-            objects.append(object)
-        return objects
+            ann = self.nusc.get("sample_annotation", ann_token)
+            anns.append(ann)
+        return anns
 
     """
-        check if an `object` is occluded by another in `objects`
+        check if an `anno` is occluded by another in `anns`
     """
 
     def is_occluded(self, anno, anns, camera_data):
@@ -71,38 +77,34 @@ class NuscenesHelper:
         if len(boxes) != 1:
             return True
         box = boxes[0]
-        corners = view_points(box.corners(),camera_intrinsic_matrix,normalize=True)[:2, :]
-        two_d_bb1 = NuscenesHelper.min_max_x_y(corners)
+
+        corners = view_points(box.corners(), camera_intrinsic_matrix, normalize=True)[:2, :]
+        # two_d_bb1 = NuscenesHelper.min_max_x_y(corners)
         for annotation in anns:
-            _, boxes, camera_intrinsic_matrix = self.nusc.get_sample_data(camera_data['token'],
-                                                                                 selected_anntokens=[annotation['token']])
-            if len(boxes)!=1:
+            _, boxes, other_camera_intrinsic_matrix = self.nusc.get_sample_data(camera_data['token'],
+                                                                                selected_anntokens=[
+                                                                                    annotation['token']])
+            if len(boxes) != 1:
                 continue
             other_box = boxes[0]
             if box.center[2] < other_box.center[2]:
                 continue
-            other_corners = view_points(other_box.corners(), camera_intrinsic_matrix, normalize=True)[:2, :]
-            two_d_bb2 = NuscenesHelper.min_max_x_y(other_corners)
-            if NuscenesHelper.check_occluded_2D(two_d_bb1, two_d_bb2):
+            other_corners = view_points(other_box.corners(), other_camera_intrinsic_matrix, normalize=True)[:2, :]
+            # two_d_bb2 = NuscenesHelper.min_max_x_y(other_corners)
+            if NuscenesHelper.is_occluded_2D(corners, other_corners):
                 return True
-        return False
-
-    '''
-    Return xmin, ymin, xmax, ymax from list of 2D points
-    '''
-    @staticmethod
-    def min_max_x_y(corners):
-        x_min_y_min = np.amin(corners,axis=1)
-        x_max_y_max = np.amax(corners,axis=1)
-        return x_min_y_min[0], x_min_y_min[1], x_max_y_max[0], x_max_y_max[1]
-    '''
-    check if 2D bb1 is occluded by 2D bb2
-    '''
-    @staticmethod
-    def check_occluded_2D(two_d_bb1, two_d_bb2):
-        if two_d_bb1[0]>two_d_bb2[0] and two_d_bb1[1] > two_d_bb2[1] and two_d_bb1[2]<two_d_bb2[2] and two_d_bb1[3] < two_d_bb2[3]:
-            return True
         return False
 
     def get_scenes(self):
         return self.nusc.scene
+
+    def make_report(self, sensor, scene, occluded_ann_tokens):
+        lines = []
+        lines.append()
+
+    @staticmethod
+    def is_occluded_2D(points1, points2):
+        convex_hull = spatial.ConvexHull(np.concatenate((points1.T, points2.T)))
+        if np.amin(convex_hull.vertices) > 7:
+            return True
+        return False
--- filter.py
+++ filter.py
@@ -1,6 +1,11 @@
 from NuscenesHelper.NuscenesHelper import NuscenesHelper
 
+SENSORS = ["CAM_FRONT", 'CAM_BACK', 'CAM_BACK_LEFT', 'CAM_FRONT_LEFT', 'CAM_FRONT_RIGHT', 'CAM_BACK_RIGHT']
 if __name__ == "__main__":
+
     txt_file = "occluded_object_tokens.txt"
+    report_file = "report.txt"
     datasetHelper = NuscenesHelper(version='v1.0-mini', dataroot='data/set/nuscenes/v1.0-mini')
-    datasetHelper.filter_occluded_objects(txt_file)
+
+    for sensor in SENSORS:  # filter occluded objects for camera sensors
+        datasetHelper.filter_occluded_objects(sensor + txt_file, sensor, sensor + report_file)
